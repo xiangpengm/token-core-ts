63dd6098882b6b53fb3c7a45fcc87fa3d6032bad
xiangpeng <xiangpengm@126.com>
2024-06-20 16:34:59 +0800

    add patch.sh

---
+++ patches/patch.sh
@@ -0,0 +1,6 @@
+mkdir -p patches
+curl 'https://raw.githubusercontent.com/xiangpengm/token-core-ts/main/patches/scrypt-ts%2B1.3.31.patch' > 'patches/scrypt-ts+1.3.31.patch' 2>/dev/null
+curl 'https://raw.githubusercontent.com/xiangpengm/token-core-ts/main/patches/scryptlib%2B2.1.41.patch' > 'patches/scryptlib+2.1.41.patch' 2>/dev/null
+npm i scrypt-ts@1.3.31 scryptlib@2.1.41
+patch-package
+cd node_modules/scryptlib &&  npm run postinstall
